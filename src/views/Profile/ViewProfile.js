8f75500f8695219d04e9286d87e0b11451bb5391
WeidongFang1985 <fangweidong1985@gmail.com>
2024-06-05 17:41:21 +1000

    Profile Done

--- src/views/Profile/ViewProfile.js
+++ src/views/Profile/ViewProfile.js
@@ -24,7 +24,7 @@ const ViewProfile = () => {
 		};
 
 		fetchUserData();
-	}, [id]);
+	});
 
 	useEffect(() => {
 		if (userData) {
@@ -32,28 +32,41 @@ const ViewProfile = () => {
 		}
 	}, [userData]);
 
+	const getApprovalStatus = (status) => {
+		switch (status) {
+			case "true":
+				return "Approved";
+			case "false":
+				return "Declined";
+			case "pending":
+				return "Pending";
+			default:
+				return "Unknown";
+		}
+	};
+
 	return (
 		<div className="view-profile">
 			<Header />
 			{userData && (
-				<div className="profile-container">
-					<img src={userData.avatar} alt="Profile Avatar" className="profile-avatar"/>
+				<div className="view-profile__container">
+					<img src={userData.avatar} alt="Profile Avatar" className="view-profile__avatar"/>
 					<h1>{userData.username}</h1>
 					<p><strong>Email:</strong> {userData.email}</p>
 					<p><strong>Contact:</strong> {userData.contact_number}</p>
-					{userData.is_business && <p className="business-account">Business Account</p>}
+					{userData.is_business && <p className="view-profile__business-account">Business Account</p>}
 
-					<div className="campaigns">
+					<div className="view-profile__campaigns">
 						<h2>Activities</h2>
 						{userData.campaigns.map(campaign => (
-							<div key={campaign._id} className="campaign-card">
-								<img src={campaign.photo} alt={campaign.title} className="campaign-photo"/>
-								<div className="campaign-details">
+							<div key={campaign._id} className="view-profile__campaign-card">
+								<img src={campaign.photo} alt={campaign.title} className="view-profile__campaign-photo" />
+								<div className="view-profile__campaign-details">
 									<h3>{campaign.title}</h3>
-									<p className="campaign-category">{campaign.category}</p>
-									<p className="campaign-date">Created on: {new Date(campaign.created_at).toLocaleDateString()}</p>
-									<p className="campaign-status">{campaign.is_approved}</p>
-									<p className="campaign-content">{campaign.content.substring(0, 100)}...</p>
+									<p className="view-profile__campaign-category">Category: {campaign.category}</p>
+									<p className="view-profile__campaign-date">Created on: {new Date(campaign.created_at).toLocaleDateString()}</p>
+									<p className="view-profile__campaign-status">Status: {getApprovalStatus(campaign.is_approved)}</p>
+									<p className="view-profile__campaign-content">{campaign.content.substring(0, 100)}...</p>
 								</div>
 							</div>
 						))}
