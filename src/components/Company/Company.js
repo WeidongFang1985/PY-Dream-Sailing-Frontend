ec13f2caad829decda24b14ec4d0907405903e8a
WeidongFang1985 <fangweidong1985@gmail.com>
2024-06-21 11:53:21 +1000

    company get info success

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './Company.css';
+import {getAllCompanies} from "../../services/getAllCompanies";
+
 
 const Company = () => {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        const fetchedCompanies = await getAllCompanies();
+        setCompanies(fetchedCompanies)
+      } catch (error) {
+        console.error('Error fetching campaigns:', error);
+      }
+    };
+
+    fetchCompanies();
+  }, []);
+
+  console.log(companies)
   return (
-    <div>
-      <h2>Sponsors</h2>
+    <div className="company">
+      <h3>Sponsors</h3>
+      {companies.length > 0 ? (
+        <ul className="campaigns">
+          {companies.map(company => (
+            <li key={company._id} >
+              <img src={company.photo} alt="companyPhoto" width={40}/>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No campaigns available.</p>
+      )}
     </div>
   );
 };
